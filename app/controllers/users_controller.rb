a509121b9db201845c40c1e16795352e5d278c3b
Nbeanavides23 <franco.noly@gmail.com>
2018-09-05 17:56:57 -0400

    Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    
+    def index
+        @user = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+        redirect_to '/'
+
+    end
+
+    def new
+        @user - User.new
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+    end 
+
+    def create
+        @user = User.find(params[:id])
+    end
+
+
+   
 end
