ecc55ce12c0a00bdaf9dc60c18b006e2fac86f3a
Tom Barnes <barnes.thomas.c@gmail.com>
2018-09-05 17:01:47 -0400

    added comments model and admin seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_195217) do
+ActiveRecord::Schema.define(version: 2018_09_05_201332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "building_id"
+    t.string "type"
+    t.integer "rating"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
