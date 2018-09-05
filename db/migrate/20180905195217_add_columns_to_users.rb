02f891a2da8c1fe2c8478c3334d1ed0c976f7df5
Tom Barnes <barnes.thomas.c@gmail.com>
2018-09-05 17:01:47 -0400

    added comments model and admin seed data

--- db/migrate/20180905195217_add_columns_to_users.rb
+++ db/migrate/20180905195217_add_columns_to_users.rb
@@ -5,5 +5,5 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :birthdate, :date
     add_column :users, :is_admin, :boolean
     add_column :users, :username, :string
-  end
+    end
 end
