518f5a5b0c2ae2eee6500145d272ebce329a162a
Tom Barnes <barnes.thomas.c@gmail.com>
2018-09-05 17:01:47 -0400

    added comments model and admin seed data
    commit 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_214918) do
+ActiveRecord::Schema.define(version: 2018_09_07_200945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
