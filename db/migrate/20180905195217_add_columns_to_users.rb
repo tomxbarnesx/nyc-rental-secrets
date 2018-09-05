64ff575953facf658e99d9d4ca448eb7391a6088
Tom Barnes <barnes.thomas.c@gmail.com>
2018-09-05 16:02:41 -0400

    added users migration

---
+++ db/migrate/20180905195217_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birthdate, :date
+    add_column :users, :is_admin, :boolean
+    add_column :users, :username, :string
+  end
+end
