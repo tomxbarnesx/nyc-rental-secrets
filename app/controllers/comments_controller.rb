2d7e4148c17249fde02dbdf4f44e3c9ca5c87394
Anna <anna.brakowska91@gmail.com>
2018-09-07 16:24:32 -0400

    implemented creating new comments with building number pulled from ajax call

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,18 @@ class CommentsController < ApplicationController
 
     def create
         @comment = Comment.new(comment_params)
+
+        if @comment.save
+           redirect_to '/buildings/show'
+       else
+           render 'new'
+      end
+
     end
 
 
 
     def comment_params
-        params.require(:comment).permit(:user_id, :building_id, :type, :rating, :body)
+        params.require(:comment).permit(:user_id, :building_id, :comment_type, :rating, :body)
     end
 end
