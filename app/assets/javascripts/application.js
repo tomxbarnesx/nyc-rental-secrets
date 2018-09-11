feb20b65bda1cf150aa9fb2723c6104ae46419d9
Anna <anna.brakowska91@gmail.com>
2018-09-11 10:06:12 -0400

    added autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,37 @@ $(document).ready(function() {
   $(".menu .item").tab({ history: false });
 });
 
+var placeSearch, autocomplete, geocoder;
+
+function initAutocomplete() {
+  geocoder = new google.maps.Geocoder();
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete") /*,
+      {types: ['(cities)']}*/
+  );
+
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function codeAddress(address) {
+  geocoder.geocode({ address: address }, function(results, status) {
+    if (status == "OK") {
+      alert(results[0].geometry.location);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+let search;
+
+function fillInAddress() {
+  var place = autocomplete.getPlace();
+  search = document.getElementById("autocomplete").value;
+
+  //   codeAddress(document.getElementById('autocomplete').value);
+}
+
 function initMap() {
   let main = {
     zoom: 8,
